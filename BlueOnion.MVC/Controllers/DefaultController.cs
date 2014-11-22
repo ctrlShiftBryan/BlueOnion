08ec7fe522944757ad68ba0b051b0a1ada0db51b
Bryan Arendt <bryan@dynamit.com>
2014-11-22 09:16:14 -0500

    mvc - add views

---
+++ BlueOnion.MVC/Controllers/DefaultController.cs
@@ -0,0 +1,91 @@
+﻿using BlueOnion.ViewModel.Interfaces;
+using System;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BlueOnion.MVC.Controllers
+{
+    public class AuthenticationController : BaseController
+    {
+        public AuthenticationController(IViewModelServices services) : base(services)
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            var authCookie = Response.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null && !string.IsNullOrEmpty(authCookie.Value)) return RedirectToLocal(returnUrl);
+
+            var OnQ_APPURL = GetAuthenticationUrl(returnUrl);
+
+            if (!AllowDevLogin)
+            {
+                if (string.IsNullOrEmpty(returnUrl))
+                    returnUrl = "/";
+
+                if (User.Identity.IsAuthenticated)
+                    return new RedirectResult(returnUrl);
+
+                return new RedirectResult(OnQ_APPURL);
+
+            }
+
+            if (string.IsNullOrEmpty(returnUrl))
+                returnUrl = "/";
+
+            if (User.Identity.IsAuthenticated)
+                return RedirectToLocal(returnUrl);
+
+            return View(); 
+            //todo return proper user view model for login
+            //return View(new UserSecurityProfileViewModel { ReturnUrl = returnUrl });
+        }
+
+        private bool AllowDevLogin
+        {
+            get
+            {
+                var testLogin = false;
+                bool.TryParse(WebConfigurationManager.AppSettings["AllowDevLogin"], out testLogin);
+
+                return testLogin;
+            }
+        }
+
+        private string GetAuthenticationUrl(string returnUrl)
+        {
+            var baseUrl = ConfigurationManager.AppSettings["SiteRoot"];
+            if (String.IsNullOrEmpty(returnUrl)) return ConfigurationManager.AppSettings["AuthenticationSiteRoot"];
+            return returnUrl.IndexOf(baseUrl) > -1 ?
+
+               String.Format("{0}{1}",
+                       ConfigurationManager.AppSettings["AuthenticationSiteRoot"],
+                       returnUrl
+                   )
+               :
+
+               String.Format("{0}{1}{2}",
+                       ConfigurationManager.AppSettings["AuthenticationSiteRoot"],
+                       baseUrl,
+                       returnUrl
+                   );
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+
+            return RedirectToAction("Index", "MaintenanceTable");
+        }
+    }
+}
--- BlueOnion.MVC/Global.asax.cs
+++ BlueOnion.MVC/Global.asax.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using BlueOnion.Repository.Common;
+using BlueOnion.ViewModel.AutoMapper;
+using BlueOnion.ViewModel.Interfaces;
+using System;
+using System.Data.Entity;
+using System.Security.Principal;
+using System.Threading;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
-using System.Web.SessionState;
-using System.Web.Http;
 
 namespace BlueOnion.MVC
 {
@@ -14,10 +18,35 @@ namespace BlueOnion.MVC
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            UnityConfig.RegisterComponents();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ConfigMapper.MapAll();
+            Database.SetInitializer<BlueOnionContext>(null); ;           
+        }
+
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            var authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie == null || string.IsNullOrEmpty(authCookie.Value)) return;
+            var authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+            var domainServices = DependencyResolver.Current.GetService(typeof(IDomainServices)) as IDomainServices;
+            //to use user service
+            //var userCommandService = domainServices.UserSecurityProfileQueryService;
+            //var user = userCommandService.GetByUsername(authTicket.Name);
+            //var roles = user.UserSecurityProfileRoles.Select(x => x.Role.Name).ToArray();
+            var roles = new string[0];
+            var genericIdentity = new GenericIdentity(authTicket.Name);
+
+            var genericPrincipal = new GenericPrincipal(genericIdentity, roles);
+
+            Thread.CurrentPrincipal = genericPrincipal;
+            Context.User = genericPrincipal;
         }
     }
+
+
 }
